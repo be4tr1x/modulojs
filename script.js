7f2f4a7556816ccdf063fb1e9129c11bdb54adfb
Beatriz Souza <biahph21@gmail.com>
2020-09-29 10:14:06 -0300

    Arrow Functions até Keys, Values e Entries

--- script.js
+++ script.js
@@ -1,121 +1,121 @@
-/*
-let number = Math.floor(Math.random() * 100);
-console.log(number);
 
+//Arrow Functions
 
+function somar(x, y){
 
-let timer;
-
-function comecar(){
-
- timer = setInterval(showTime, 1000);
+    return x + y;       // -> método básico
 
 }
+console.log(somar(25, 60))
 
-function parar(){
 
- clearInterval(timer);
+ let subtrair = (x, y) => x - y;
+
+ console.log(subtrair(6, 4))
+
+
+ let letrasNoNome = (nome) => {
+    return nome.length
+ }
+ console.log(letrasNoNome('Beatriz'))
+
+
+ //Operador Spread
+
+ let numeros =[1, 2, 3, 4 ];
+
+ let outros= [...numeros, 5, 6, 7, 8];
+
+console.log(outros)
+
+let info = {
+
+    nome: 'beatriz',
+    sobrenome:'souza',
+    idade: 16
+
+};
+
+let novaInfo = {
+    ...info,
+    cidade: "ananindeua",
+    estado: 'pará',
+    país: 'brasil'
+
+};
+
+console.log(novaInfo);
+
+
+function adicionarInfo(info){
+
+    let novasInfo = { 
+        ...info,
+        status: 0,
+        token: 'AFBKH1',
+        data_cadastro: '12/12/2012',
+    };
+
+    return novasInfo;
+
 
 }
+console.log(adicionarInfo({nome: 'Valentina', sobrenome:'Enzo'}));
 
-function showTime(){
+//Operador Rest
 
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let txt = h+':'+ m+':'+ s;
 
-  document.querySelector('.demo').innerHTML = txt;
+function adicionar(...number){ // ele faz que pegue os demais parametros 
 
+    console.log(number)
 }
 
-function sorteio(){
-
-    let number = Math.floor(Math.random() * 50,80, 90, 12,);
-    document.querySelector(".number").innerHTML = number
+adicionar(1,2,3,4)
 
 
+//JUNTANDO O OPERADOR SPREAD E REST
+
+function list(nomes, ...novosNomes){
+   
+    let novoConjunto = [
+
+        ...nomes,
+        ...novosNomes
+    ]
+
+    return novoConjunto
 }
+
+let nomes = ['katia', 'flavia'];
+
+let outros = list(nomes, 'pedro', 'gaga');
   
-//Template String
-
-let nome = "Beatriz";
- 
-let idade = 16;
-
-//let frase = "Meu nome é "+nome+", e ano que vem eu terei "+(idade +1)+ "anos"
-let frase = `Meu nome é ${nome}, eu tenho ${idade} anos e ano que vem eu terei ${idade + 1} anos` ;
-console.log(frase)
-*/
-//Desconstrução de Objetos
-let pessoa = {
-
-    filme: 'Carol',
-    atriz: 'Sarah Paulson',
-    idade: 'não definido',
-    social:{
-        facebook: "Sarah",
-        instagram: '@sarah',
-        twitter:{
-            url: "n3bul0s4",
-            seguidores: 6000
-        }
-
-    },
-
-    fichaTecnica:function() {
-
-        return `${this.atriz} participou do filme ${this.filme}`;
-
-    }
-
-};
-
-//let {filme, atriz, idade, social:{twitter:{url:twitter, seguidores}}} = pessoa;
-
-//console.log(filme, atriz, idade, twitter, seguidores); 
- 
-
-let pessoas = {
-
- nome: 'Jessica',
- sobrenome: 'Lange',
- idade: 64,
- filmes: `etc, etc, etc`
-
-}
-
-function pegarNomeCompleto(obj){
-
-    return `${obj.nome} ${obj.sobrenome} `
-
-};
-
-console.log(pegarNomeCompleto(pessoas));
+console.log(outros);
 
 
-//Descontruindo Arrays
-
-let info = ['Beatriz Souza', 'Beatriz', 'Souza', '@be4tr1x'];
-
-//let [nomeCompleto, nome, sobrenome, github] = info; //pegar todos os Arrays
-
-let [nomeCompleto, ,, github ] = info;
-
-console.log(nomeCompleto, github);
+//Includes e Repeat
 
 
-function criar (){
+let lista = ['ovo', 'café', 'arroz', 'feijão', 'carne'];
 
-    let a = [1,2,3];
-    return a
+console.log(lista.includes('galinha')); // o includes retorna true ou false
 
 
-}
+console.log('x'.repeat(12)) // repeat retona x vezes o número desejado
 
-let numeros = criar();
 
-let [a,b,c] = numeros;
+//KEY, VALUES E ENTRIES EM ARRAY
+let listaTelefonica =[852456, 7965, 7951563];
 
-console.log(a,b,c);
+console.log(Object.keys(listaTelefonica))// keys 
+
+let foods = ['pizza','batata frita', 'refrigerante'];
+
+console.log(Object.values(foods)); //values
+
+let animais = ['galinha', 'pato', 'vaca'];
+
+console.log(Object.entries(animais));
+
+
+// KEYS = EM OBJETOS RETORNA TODAS AS CHAVES DENTRO FO OBJ
